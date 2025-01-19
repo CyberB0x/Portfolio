636d61fd8d73fe584a63e49272e935de3de22060
Arslon <arslonbekefsun09@gmail.com>
2025-01-19 22:11:25 +0300

    file

--- Generator info/generator info for reg/main.py
+++ Generator info/generator info for reg/main.py
@@ -24,7 +24,7 @@ def generate():
 # Отображение веб-страницы
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('htmlfilename')
 
 if __name__ == "__main__":
     app.run(debug=True)
